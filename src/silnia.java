3e4ae04a67cbbf3aeb41efa2742ced1d865d5e28
Kasia <kasiag@gmx.com>
2019-08-27 00:27:38 +0200

    silnia

---
+++ src/silnia.java
@@ -0,0 +1,23 @@
+public class silnia {
+
+        public static void main(String[] args) {
+            int n = 1;
+            for (int i=1;i<=5;i++){
+
+                n=n*(i*1);
+                // 1*2 = 2
+                //       2*3 = 6
+                //             6 *4 = 24
+
+
+                //  n=(n*(n-1));
+
+                // n = nextint();read for input
+                System.out.println(n);
+
+
+            }
+            System.out.println(n);
+
+
+        }}
